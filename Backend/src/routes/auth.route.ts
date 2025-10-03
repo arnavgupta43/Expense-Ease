836c7baaf9837c6ed81f3582a3913067e44d053f
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-10-03 17:56:13 +0530

    update: data validation

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller";
+import { validate } from "../middlewares/validate";
+import { loginSchema, registerSchema } from "../validators/auth.validator";
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validate(registerSchema), register);
+router.post("/login", validate(loginSchema), login);
 export default router;
