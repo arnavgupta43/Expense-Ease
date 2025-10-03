e823fdb5615608fe8803f19fa490762f2f2f7737
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-10-03 17:22:22 +0530

    routes added

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
+import router from "./routes/auth.route";
 const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
+app.use("/auth", router);
 export default app;
