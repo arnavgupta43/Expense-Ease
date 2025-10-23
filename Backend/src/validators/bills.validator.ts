1ad060eda01199cf06d42f8bf6c12e07662e04c6
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-10-23 13:22:24 +0530

    update: validators

--- Backend/src/validators/bills.validator.ts
+++ Backend/src/validators/bills.validator.ts
@@ -2,14 +2,35 @@ import Joi from "joi";
 
 export const validatecreateBill = {
   body: Joi.object({
-    title: Joi.string().required().min(5).messages({
+    title: Joi.string().required().min(5).max(30).messages({
       "string.min": "Title should be of length 5",
       "any.required": "Title is required",
     }),
   }),
-  amount: Joi.number().required().greater(0).messages({
+  totalAmount: Joi.number().required().positive().messages({
     "any.required": "Amountis required",
     "number.base": "Amount should be a number",
   }),
-  participants: Joi.required(),
+  participants: Joi.array()
+    .items(
+      Joi.object({
+        userId: Joi.number()
+          .required()
+          .positive()
+          .integer()
+          .message("UserId should be valid"),
+        amountOwed: Joi.number()
+          .positive()
+          .required()
+          .integer()
+          .message("Amount should be correct"),
+      })
+    )
+    .unique("userId"),
+};
+
+export const upadteDeleteBill = {
+  params: Joi.object({
+    id: Joi.number().positive().required().message("Valid Bill id is required"),
+  }),
 };
