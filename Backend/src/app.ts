194d6197a6bdf1836b922319116a5e046fc46c83
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-10-28 19:41:36 +0530

    add: rate limiter

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 import userRoutes from "./routes/user.routes";
 import router from "./routes/auth.route";
 import friendRouter from "./routes/friend.route";
 import expenseRouter from "./routes/expense.route";
 import billrouter from "./routes/bills.route";
 import { notFoundRoute } from "./controllers/notFoundController";
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
 const app = express();
+app.use(cors());
+app.use(limiter);
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/auth", router);
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,13 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import app from "./app";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-
 const server = express();
-server.use(cors());
-server.use(express.json());
 server.use(app);
 
 server.listen(PORT, () => {
