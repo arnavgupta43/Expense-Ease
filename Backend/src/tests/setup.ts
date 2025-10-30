08fafcd223311d4ba9c90ac5b0f3a65b2bfef7be
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-10-30 21:38:40 +0530

    update:test setup

--- Backend/src/tests/setup.ts
+++ Backend/src/tests/setup.ts
@@ -1,8 +1,9 @@
-// This runs before each test
-jest.mock("../config/db", () => {
-  const prismaMock = require("../__mocks__/prisma").default;
-  return { __esModule: true, default: prismaMock };
+import { execSync } from "child_process";
+// Run before all tests
+beforeAll(() => {
+  execSync("dotenv -e .env.test -- npx prisma db push", { stdio: "inherit" });
 });
+
 beforeEach(() => {
   jest.clearAllMocks();
 });
